61cb6e5a893cdc720c41f25bb006dbd7243f45e9
malenarojas <malenarojascamargo@@hotmail.com>
2025-07-07 21:57:27 -0400

    fix: remove extraneous characters from greeting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <PropiedadList/>
       <AgentsList/>
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world!sfvsdvsdvsdv
     </h1>
     </div>  
   )
